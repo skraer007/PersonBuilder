9ad74567df5d043199c0db47050a7b0108547f55
Andrey Ivanov <skraer2001@mail.ru>
2023-01-08 16:36:39 +0300

    Version 1.0

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,89 @@
+package org.example;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age = -1;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        if (age == 0) {
+            return OptionalInt.empty();
+        } else {
+            return OptionalInt.of(age);
+        }
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(getSurname())
+                .setAge(0)
+                .setAddress(getAddress());
+    }
+}
